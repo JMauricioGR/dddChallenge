00517ef5fe2f36aee9db8ab257fb6390c67af937
Jehison Galarza <jehisonsc045@gmail.com>
2022-05-21 07:55:55 -0500

    DDDChallenge --- Adding file for use case of  updateServiceTime

--- src/main/java/com/sofkaU/ddd/challenge/usecase/bathandcare/AddServiceUseCase.java
+++ src/main/java/com/sofkaU/ddd/challenge/usecase/bathandcare/AddServiceUseCase.java
@@ -5,6 +5,7 @@ import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import com.sofkaU.ddd.challenge.domain.bathandcare.Service;
 import com.sofkaU.ddd.challenge.domain.bathandcare.commands.AddService;
+import com.sofkaU.ddd.challenge.domain.bathandcare.commands.UpdateServiceTime;
 import com.sofkaU.ddd.challenge.domain.bathandcare.events.ServiceAdded;
 import com.sofkaU.ddd.challenge.domain.bathandcare.values.Price;
 import com.sofkaU.ddd.challenge.domain.bathandcare.values.ServiceId;
@@ -18,7 +19,7 @@ public class AddServiceUseCase extends UseCase<RequestCommand<AddService>, Respo
     @Override
     public void executeUseCase(RequestCommand<AddService> addServiceRequestCommand) {
         var command = addServiceRequestCommand.getCommand();
-        var addservice = new Service(command.getEntityId(), command.getServiceName(), command.getServiceTime(), command.getPrice());
+        var addservice = new UpdateServiceTime(command.getEntityId(), command.getServiceTime());
 
         emit().onResponse(new ResponseEvents(List.of(new ServiceAdded(new ServiceId(), new ServiceName("Bathing"),new ServiceTime("1 hour"), new Price("$65.000")))));
     }
--- src/main/java/com/sofkaU/ddd/challenge/usecase/bathandcare/UpdateServiceTmeUseCase.java
+++ src/main/java/com/sofkaU/ddd/challenge/usecase/bathandcare/UpdateServiceTmeUseCase.java
@@ -3,13 +3,24 @@ package com.sofkaU.ddd.challenge.usecase.bathandcare;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
+import com.sofkaU.ddd.challenge.domain.bathandcare.Service;
 import com.sofkaU.ddd.challenge.domain.bathandcare.commands.UpdateServiceTime;
+import com.sofkaU.ddd.challenge.domain.bathandcare.events.ServiceTimeUpdated;
+import com.sofkaU.ddd.challenge.domain.bathandcare.values.ServiceId;
+import com.sofkaU.ddd.challenge.domain.bathandcare.values.ServiceTime;
+
+import java.util.List;
 
 public class UpdateServiceTmeUseCase extends UseCase<RequestCommand<UpdateServiceTime>, ResponseEvents> {
 
 
     @Override
     public void executeUseCase(RequestCommand<UpdateServiceTime> updateServiceTimeRequestCommand) {
+
+        var command = updateServiceTimeRequestCommand.getCommand();
+        var updateTime = new Service(command.getServiceId(), command.getServiceTime());
+
+        emit().onResponse(new ResponseEvents(List.of(new ServiceTimeUpdated(new ServiceId(),new ServiceTime("2 hours")))));
 
     }
 }
